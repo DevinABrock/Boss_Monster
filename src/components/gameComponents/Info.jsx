e7e133773f2600ac4cbf90b268d988b7e94e4d74
MercerMahaffey <89871123+MercerMahaffey@users.noreply.github.com>
2021-12-21 11:51:03 -0500

    adding heroes to town

--- src/components/gameComponents/Info.jsx
+++ src/components/gameComponents/Info.jsx
@@ -1,7 +1,75 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import '../css/Info.css'
+import { nextGamePhase, dealHeroesToTown } from '../../actions/sampleActions';
+import { diceRoll } from '../gameLogic/diceRoll';
+
 
 function Info() {
+
+    const dispatch = useDispatch();
+
+    const gamePhase = useSelector(state => state.gamePhase.gamePhase)
+    const gameRound = useSelector(state => state.gamePhase.gameRound)
+    const playerRoomHand = useSelector(state => state.cardDecks.playerRoomHand)
+
+    const [switchRanThisGamePhase, setSwitchRanThisGamePhase] = useState(false);
+    
+    const handleChangeGamePhase = () => {
+        // if 1 and player has rooms in their hand
+        if(gamePhase===1 && playerRoomHand.length){
+            dispatch(nextGamePhase())
+        }
+        // if 2 and user clicks next this will update whether a spell card takes effect for the round
+        if(gamePhase===2){
+            dispatch(nextGamePhase())
+        }
+    }
+
+    const renderMessageSwitch = (gamePhase) => {
+        console.log('running switch');
+            switch (gamePhase) {
+                case 1:
+                    
+                    return <div className='messageBox'><div className='message'>Build a room in your dungeon!</div></div>
+                case 2:
+                    
+                    let {rollNumber, isHit} = diceRoll(gameRound);
+                    
+                    return <div className='messageBox'>
+                        <div className='message'>Rolling a dice to see if you are hit by spell!</div>
+                        <div className='message'>Dice Result: {rollNumber}</div>
+                        {isHit? <div className='message'>You are Hit!</div>: <div className='message'>You manage to avoid the spell!</div>}
+                        </div>
+                case 3:
+                    if(!switchRanThisGamePhase){
+                        setSwitchRanThisGamePhase(true)
+                        dispatch(dealHeroesToTown('ordinary', 2))
+                    }
+                    return <div className='messageBox'><div className='message'>Adventurers wandering into Town.</div></div>
+                default:
+                    break;
+            }
+    }
+    const renderGamePhaseSwitch = (gamePhase) => {
+        switch (gamePhase) {
+            case 1:
+                return `Pre-Game Phase ${gamePhase}`
+            case 2:
+                return `Dice-Roll Phase ${gamePhase}`
+            case 3:
+                return `Dealing Heroes Phase ${gamePhase}`
+            default:
+                break;
+        }
+    }
+
+    const handleNextButtonClick = () => {
+        
+        handleChangeGamePhase();
+        setSwitchRanThisGamePhase(false);
+    }
+
     return (
         <div className='infoBody'>
 
@@ -21,9 +89,9 @@ function Info() {
 
             {/* -- BUTTON AREA -- */}
             <div className='buttonArea'>
-                <div className='phaseInfo'>Phase: Adventure</div>
+                <div className='phaseInfo'>Phase: {renderGamePhaseSwitch(gamePhase)}</div>
                 <div className='buttonList'>
-                    <div className='button'>NEXT</div>
+                    <div onClick={()=>handleNextButtonClick()} className='button'>Next</div>
                     <div className='button'>BACK</div>
                     <div className='button'>BUILD</div>
                     <div className='button'>STORE</div>
@@ -32,13 +100,14 @@ function Info() {
 
             {/* -- MESSAGE AREA -- */}
             <div className='messageArea'>
-                <div className='messageBox'>
-                    <div className='message'>:New Heroes Are In Town.</div>
+                    {renderMessageSwitch(gamePhase)}
+                {/* <div className='messageBox'> */}
+                    {/* <div className='message'>:New Heroes Are In Town.</div>
                     <div className='message'>:Golem Factory Has Been Built!</div>
                     <div className='message'>:You Are Now In Adventure Phase. Happy Hunting.</div>
                     <div className='message'>:Mage Killed! 1 Soul Acquired.</div>
-                    <div className='message'>:You Have Been Wounded!</div>
-                </div>
+                    <div className='message'>:You Have Been Wounded!</div> */}
+                {/* </div> */}
             </div>
         </div>
     )
