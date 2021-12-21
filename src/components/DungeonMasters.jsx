7d682354b130dd46d135baebf79062572f8deb70
Devin Brock <devinabrock@gmail.com>
2021-12-21 17:45:27 -0500

    save

--- src/components/DungeonMasters.jsx
+++ src/components/DungeonMasters.jsx
@@ -6,46 +6,37 @@ function DungeonMasters() {
         <div className='dmBody'>
             {/* -- TITLE AREA -- */}
             <div className="dmBanner">
-                <img className="imgOne" src='/homeBannerNoWords.png' alt="" />
-                <div className='dmTitle'>
-                    <div className='dmTitle1'>DUNGE<img className='dmIcon' src='/favicon.ico' />N</div>
-                    <div className='dmTitle2'>MASTERS</div>
-                </div>
+                <img className="dmBackground" src='/dung_mast_background.png' alt="" />
+                    <div className='dmTitle'>DUNGE<img className='dmIcon' src='/favicon.ico' />N MASTERS</div>
 
                 {/* -- LEADERBOARD AREA -- */}
                     <div className='leaderBoardContainer'>
-                        {/* -- SEPERATE USER -- */}
                         <div className='dmGuide'>
-                            <div className='dmGuideItem'>User</div>
-                            <div className='dmGuideItem'>W/L</div>
-                            <div className='dmGuideItem'>Souls</div>
-                            <div className='dmGuideItem'>Rank</div>
+                            <div className='dmRank'>Rank</div>
+                            <div className='dmUsername'>User</div>
+                            <div className='dmWL'>W/L</div>
+                            <div className='dmSouls'>Souls</div>
+                            <div className='dmBadge'>Badge</div>
                         </div>
-                        <div className='dmUsers'>
+                        <div className='dmUserDisplay'>
+
+                        {/* -- SEPERATE USER -- */}
                             <div className='dmUserItem'>
-                                <div className='dmUserInfo'>Username</div>
-                                <div className='dmUserInfo'>L</div>
-                                <div className='dmUserInfo'>4</div>
-                                <div className='dmUserInfo'>Legend</div>
+                                <div className='dmRank'>1</div>
+                                <div className='dmUsername'>Username</div>
+                                <div className='dmWL'>L</div>
+                                <div className='dmSouls'>4</div>
+                                <div className='dmBadge'>Legend</div>
                             </div>
+
                             <div className='dmUserItem'>
-                                <div className='dmUserInfo'>Username</div>
-                                <div className='dmUserInfo'>L</div>
-                                <div className='dmUserInfo'>4</div>
-                                <div className='dmUserInfo'>Legend</div>
+                                <div className='dmRank'>2</div>
+                                <div className='dmUsername'>Username</div>
+                                <div className='dmWL'>W</div>
+                                <div className='dmSouls'>4</div>
+                                <div className='dmBadge'>Legend</div>
                             </div>
-                            <div className='dmUserItem'>
-                                <div className='dmUserInfo'>Username</div>
-                                <div className='dmUserInfo'>L</div>
-                                <div className='dmUserInfo'>4</div>
-                                <div className='dmUserInfo'>Legend</div>
-                            </div>
-                            <div className='dmUserItem'>
-                                <div className='dmUserInfo'>Username</div>
-                                <div className='dmUserInfo'>L</div>
-                                <div className='dmUserInfo'>4</div>
-                                <div className='dmUserInfo'>Legend</div>
-                            </div>
+                            
                         </div>
                     </div>
             </div>
