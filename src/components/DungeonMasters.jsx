e6df3fa60348c4798b767b3c0d4afd08a88dabea
Devin Brock <devinabrock@gmail.com>
2021-12-21 14:51:21 -0500

    save

--- src/components/DungeonMasters.jsx
+++ src/components/DungeonMasters.jsx
@@ -4,12 +4,26 @@ import './css/DungeonMasters.css'
 function DungeonMasters() {
     return (
         <div className='dmBody'>
+            {/* -- TITLE AREA -- */}
             <div className="dmBanner">
                 <img className="imgOne" src='/homeBannerNoWords.png' alt="" />
                 <div className='dmTitle'>
-                    <div className='homeButton'>DUNGE<img className='dmIcon' src='/favicon.ico' />N</div>
-                    <div className='homeButton'>MASTERS</div>
+                    <div className='dmTitle1'>DUNGE<img className='dmIcon' src='/favicon.ico' />N</div>
+                    <div className='dmTitle2'>MASTERS</div>
                 </div>
+
+                {/* -- LEADERBOARD AREA -- */}
+                    <div className='leaderBoardContainer'>
+                        <div className='dmGuide'>
+                            <div className='dmGuideItem'>User</div>
+                            <div className='dmGuideItem'>W/L</div>
+                            <div className='dmGuideItem'>Souls</div>
+                            <div className='dmGuideItem'>Rank</div>
+                        </div>
+                        <div className='dmUser'>
+                            <div className='dmUserItem'></div>
+                        </div>
+                    </div>
             </div>
         </div>
     )
